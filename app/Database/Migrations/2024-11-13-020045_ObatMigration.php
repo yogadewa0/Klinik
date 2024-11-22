be7e55df748c5241c8c45f634bdd3857269150ba
angelica030404 <angelicaaprilia3@gmail.com>
2024-11-22 13:14:37 +0700

    Menambahkan isi 020045_ObatMigration Menambahkan 055121_UserMigration beserta isinya

--- app/Database/Migrations/2024-11-13-020045_ObatMigration.php
+++ app/Database/Migrations/2024-11-13-020045_ObatMigration.php
@@ -8,11 +8,44 @@ class Obat extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id_obat'          => [
+                'type'           => 'VARCHAR',
+                'constraint'     => 50,
+                'null'           => false,
+            ],
+            'nama'             => [
+                'type'           => 'VARCHAR',
+                'constraint'     => 100,
+                'null'           => false,
+            ],
+            'ukuran'           => [
+                'type'           => 'VARCHAR',
+                'constraint'     => 50,
+                'null'           => false,
+            ],
+            'satuan'           => [
+                'type'           => 'VARCHAR',
+                'constraint'     => 20,
+                'null'           => false,
+            ],
+            'tgl_kadaluarsa'   => [
+                'type'           => 'DATETIME',
+                'null'           => false,
+            ],
+            'harga'            => [
+                'type'           => 'INT',
+                'null'           => false,
+                'unsigned'       => true,
+            ],
+        ]);
+
+        $this->forge->addKey('id_obat', true); // Menetapkan id_obat sebagai primary key
+        $this->forge->createTable('obat'); // Membuat tabel obat
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('obat'); // Menghapus tabel obat saat migrasi dihapus
     }
 }
