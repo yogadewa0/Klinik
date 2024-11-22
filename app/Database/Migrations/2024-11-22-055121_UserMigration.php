1a0e6414d61031a2cc496d17c9f5fc9cd9cdcfb5
angelica030404 <angelicaaprilia3@gmail.com>
2024-11-22 13:18:21 +0700

    Menambahkan 055121_UserMigration dan isinya

--- app/Database/Migrations/2024-11-22-055121_UserMigration.php
+++ app/Database/Migrations/2024-11-22-055121_UserMigration.php
@@ -8,11 +8,40 @@ class UserMigration extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id_user'       => [
+                'type'           => 'VARCHAR',
+                'constraint'     => 50,
+                'null'           => false,
+            ],
+            'nama'          => [
+                'type'           => 'VARCHAR',
+                'constraint'     => 100,
+                'null'           => false,
+            ],
+            'alamat'        => [
+                'type'           => 'VARCHAR',
+                'constraint'     => 255,
+                'null'           => false, // Jika alamat wajib diisi
+            ],
+            'no_telp'      => [
+                'type'           => 'VARCHAR',
+                'constraint'     => 15,
+                'null'           => false, // Jika no_telp wajib diisi
+            ],
+            'role'          => [
+                'type'           => 'VARCHAR',
+                'constraint'     => 20,
+                'null'           => false, // Jika role wajib diisi
+            ],
+        ]);
+
+        $this->forge->addKey('id_user', true); // Menetapkan id_user sebagai primary key
+        $this->forge->createTable('users'); // Membuat tabel users
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('users'); // Menghapus tabel users saat migrasi dihapus
     }
 }
