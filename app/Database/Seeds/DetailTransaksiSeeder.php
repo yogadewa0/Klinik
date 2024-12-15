fcd77754caee6bdd0e85731a451756b2eb56beda
sherlyy14 <sherlylistyana0110@gmail.com>
2024-12-15 16:20:47 +0700

    Update Seeder Detail Transaksi

--- app/Database/Seeds/DetailTransaksiSeeder.php
+++ app/Database/Seeds/DetailTransaksiSeeder.php
@@ -9,12 +9,12 @@ class DetailTransaksiSeeder extends Seeder
     public function run()
     {
         $data = [
-            'jumlah' => '100000',
+            'jumlah' => '1',
             'kode_obat' => '05012617005632'
         ];
 
         //Query tambah detail transaksi
-        $this->db->query('INSERT INTO detail_transaksi(jumlah, kode_obat) VALUES(:jumlah:, :kode_obat:', $data);
+        $this->db->query('INSERT INTO detail_transaksi(jumlah,  kode_obat) VALUES(:jumlah:, :kode_obat:', $data);
 
         //Masukkan data ke tabel detail_transaksi
         $this->db->table('detail_transaksi')->insert($data);
